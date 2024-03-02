0177d3e665f3048ac2dc17d7d688e83e8eccf0d8
Francesco-Biscaccia <francesco.biscaccia.carrara@gmail.com>
2024-03-02 11:36:36 +0100

    read_tsp_file done.

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 
 int main(int argc, char **argv){
     if (argc < 2) {printf("Usage: %s -h/ -help/ --help/  to get some help\n", argv[0]); exit(1);}       
-	if (VERBOSE >= 10) {for (int a = 0; a < argc; a++) printf("%s ", argv[a]); printf("\n");}
+	if (VERBOSE >= 10) {for (int a = 0; a < argc; a++) printf("__log: %s ", argv[a]); printf("\n");}
 
 	uint64_t t1 = get_time(); 
 	instance inst;
 
-	parse_cli(argc,argv, &inst);     
+	parse_cli(argc,argv, &inst);
 	
-	//read_input(&inst);  
-	//if ( VRPopt(&inst) ) print_error(" error within VRPopt()");
+	read_tsp_file(&inst);  
+	//if (VRPopt(&inst)) print_error(" error within VRPopt()");
 	uint64_t t2 = get_time(); 
     
-	if(VERBOSE >= 10) printf("... TSP problem solved in %llu sec.s\n", t2-t1);  
+	if(VERBOSE >= 10) printf("__log: TSP problem solved in %lu sec.s\n", t2-t1);  
     
 	free_instance(&inst);
 	return 0; 
--- utils.c
+++ utils.c
@@ -1,9 +1,18 @@
 #include "utils.h"
 
+const char *WEIGHT_TYPE[] = {"13",
+    "EXPLICIT", 
+    "EUC_2D", "EUC_3D", 
+    "MAX_2D", "MAX_3D", 
+    "MAN_2D", "MAN_3D",
+    "CEIL_2D", 
+    "GEO", 
+    "ATT", 
+    "XRAY1", "XRAY2"};
+
 void free_instance(instance* inst){
-    if(VERBOSE >= 10) printf("Deallocating instance's inputs\n");
+    if(VERBOSE >= 10) printf("__log: deallocating instance's inputs\n");
     free(inst->points);
-    free(inst);
 }
 
 void print_error(const char *err){
@@ -16,10 +25,69 @@ uint64_t get_time(){
     return (uint64_t) time(NULL);
 }
 
-void read_tsp_file(instance * inst){};
+void read_tsp_file(instance * inst){
+    FILE *fin = fopen(inst->file_name, "r");
+    if ( fin == NULL ) print_error(" input file not found!");
+ 
+    inst->nnodes = 0;
+
+    char line[251];
+    char line_cp[251];
+    char node_section = 0;
+    char *par_name;   
+    char *token;
+
+    while(fgets(line, sizeof(line), fin) != NULL){
+        if (VERBOSE >= 10) { printf("__log: line: %s",line); fflush(NULL); }
+        if (strlen(line) <= 1) continue; 
+        strcpy(line_cp, line);
+        par_name = strtok(line, " :");
+        if ( VERBOSE >= 10 ) {printf("__log: parameter \"%s\" \n",par_name); fflush(NULL); }
+
+        if (!strncmp(par_name, "TYPE", 4)) {
+            if (strncmp(strtok(NULL, " :"), "TSP",3)) print_error(" format error:  only TYPE == TSP implemented!"); 
+        }
+        
+        if (!strncmp(par_name, "DIMENSION", 9)){
+            if (inst->nnodes > 0) print_error(" repeated DIMENSION section in input file");
+            inst->nnodes = atoi(strtok(NULL, " :"));
+            if (VERBOSE>=10) printf("__log: nnodes %d\n", inst->nnodes); 
+            inst->points = (point *) calloc(inst->nnodes, sizeof(point));
+        }
+
+        if (!strncmp(par_name, "EDGE_WEIGHT_TYPE", 16)){
+            int i;
+            token = strtok(NULL, " :");
+            token[strlen(token)-1]=0;
+            for(i=1;i < atoi(WEIGHT_TYPE[0]);i++){
+                if(!strncmp(token, WEIGHT_TYPE[i],strlen(token))){
+                    strcpy(inst->weight_type,WEIGHT_TYPE[i]);
+                    break;
+                }
+                
+            }
+            if(i == atoi(WEIGHT_TYPE[0])) print_error(" format error:  this format is not valid for TSP problem!"); 
+            if (VERBOSE>=10) printf("__log: weight type %s\n", inst->weight_type); 
+        }
+
+        if (!strncmp(par_name, "NODE_COORD_SECTION", 18)) node_section=1;
+        if (!strncmp(par_name, "EOD", 3)) node_section=0;
+
+        if(node_section){
+            int i = atoi(strtok(line_cp, " "))-1;
+            if ( i < 0 || i >= inst->nnodes ) continue;
+            token = strtok(NULL, " ");
+            inst->points[i].x = atof(token);
+            token = strtok(NULL, " ");
+            inst->points[i].y = atof(token);
+            if (VERBOSE>=10) printf("__log: node %d at coordinates (%15.7lf, %15.7lf)\n", i+1, inst->points[i].x,inst->points[i].y);
+        }
+
+    }
+}
 
 void parse_cli(int argc, char** argv, instance *inst){
-    if ( VERBOSE >= 10 ) printf("CLI has %d pars \n", argc-1);
+    if ( VERBOSE >= 10 ) printf("__log: CLI has %d pars \n", argc-1);
 
     //Default init of an instance
     inst->nnodes=0;
@@ -30,17 +98,14 @@ void parse_cli(int argc, char** argv, instance *inst){
     inst->best_time = MAX_TIME;
     inst->best_sol = NULL;
     strcpy(inst->file_name,"NONE");
+    strcpy(inst->weight_type,"NONE");
 
     int help = 0;
-    char no_input = 1;
 
     if (argc < 1) help = 1;
     for (int i = 1; i < argc; i++) { 
 
-		if (!strcmp(argv[i],"-in") || !strcmp(argv[i],"-f") || !strcmp(argv[i],"-file"))  {
-            strcpy(inst->file_name,argv[++i]);
-            no_input=0;
-        }
+		if (!strcmp(argv[i],"-in") || !strcmp(argv[i],"-f") || !strcmp(argv[i],"-file")) strcpy(inst->file_name,argv[++i]);
 
 		if (!strcmp(argv[i],"-tl") ||!strcmp(argv[i],"-max_time")) inst->time_limit = atof(argv[++i]);
 
@@ -67,7 +132,7 @@ void parse_cli(int argc, char** argv, instance *inst){
         printf("------Instance data------");
         printf("\n - nodes : %d",inst->nnodes);
         printf("\n - random_seed : %u",inst->random_seed);
-        printf("\n - time_limit : %llu",inst->time_limit);
+        printf("\n - time_limit : %lu",inst->time_limit);
         printf("\n - file_name : %s",inst->file_name);
         printf("\n-------------------------\n");
     }    
