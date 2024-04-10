05e91a46528a74664382cc0faf05e5046df96db9
RickSrick <riccardo.modolo.1@studenti.unipd.it>
2024-04-11 00:36:41 +0200

    quick fix g2opt

--- plot/plot_solution.py
+++ plot/plot_solution.py
@@ -6,17 +6,17 @@ xval = []
 yval = []
 
 
-f = open("../test.txt", "r")
+f = open("input/test.txt", "r")
 lines = f.readlines()
 
 for line in lines:
     v = re.findall("(.*),(.*);(.*),(.*)", line)
-    print(v)
     xval.append(float(v[0][0]))
     xval.append(float(v[0][2]))
     yval.append(float(v[0][1]))
     yval.append(float(v[0][3]))
 
 plt.plot(xval, yval, "-o")
-plt.show()
+plt.title("greedy solution")
+plt.savefig("out/test.pgf")
     
