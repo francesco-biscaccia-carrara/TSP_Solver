ced95f211e52d625a68a5d4b7ab70be04ae0cf14
RickSrick <riccardo.modolo.1@studenti.unipd.it>
2024-04-20 14:47:35 +0200

    edit plotting code

--- plot/plot_solution.py
+++ plot/plot_solution.py
@@ -1,23 +1,24 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import re
 
-xval = []
-yval = []
-
 
 f = open("input/test.txt", "r")
 lines = f.readlines()
+matplotlib.use('pgf')
 
 for line in lines:
     v = re.findall("(.*),(.*);(.*),(.*)", line)
+    xval = []
+    yval = []
     xval.append(float(v[0][0]))
     xval.append(float(v[0][2]))
     yval.append(float(v[0][1]))
     yval.append(float(v[0][3]))
+    plt.plot(xval, yval, "-o", color = "xkcd:teal")
 
-plt.plot(xval, yval, "-o")
 plt.title("greedy solution")
-#plt.savefig("out/test.pgf")
+plt.savefig("out/test.pgf", backend='pgf')
 #plt.savefig("out/test.png") Could be useful
-plt.show();   
+#plt.show();   
